60d681117b17a32e42f4134e7b3a53fb1cb5358c
Felipe Sciammarella <fl.sciammarella@ufrj.br>
2021-02-28 16:37:43 -0300

    Refactores js source for clarity and DRYness

--- index.js
+++ index.js
@@ -1,81 +1,97 @@
+const SETUP = 0, // Magic Numbers
+    COMPUTER = 1,
+    PLAYER = 2,
+    GAME_OVER = 3;
+
 var gameObject = {
-    moves: ["green", "red", "yellow", "blue"],
+    moves: ["green", "red", "yellow", "blue"], // Valid Moves
     playerMoves: [],
     computerMoves: [],
     cursor: 0,
-    level:1,
-    gamePhases: ["setup", "computer", "player", "gameOver"],
-    gamePhase: 0,
+    level: 1,
+    gamePhase: SETUP,
+}
+
+function playAudio(id="wrong") {
+    let sample = new Audio("sounds/" + id + ".mp3");
+    sample.play()
+}
+
+function animateButtonPress(target) {
+    $(target).toggleClass("pressed");
+    setTimeout(() => {
+        $(target).toggleClass("pressed");
+    }, 100);
+}
+
+function blink(targetID,duration=100){
+    console.log(targetID)
+    $("#"+targetID).fadeOut(duration).fadeIn(duration);
+} 
+
+function newMove(){
+    return gameObject.moves[Math.trunc(Math.random() * gameObject.moves.length)]
 }
 
 function gameStep() {
-    console.log(gameObject.computerMoves)
-    console.log(gameObject.playerMoves)
-    console.log(gameObject.computerMoves.length == gameObject.playerMoves.length)
-    switch (gameObject.gamePhases[gameObject.gamePhase]) {
-        case "setup":
+    switch (gameObject.gamePhase) {
+        case SETUP:
             gameObject.playerMoves = [];
             gameObject.computerMoves = [];
             gameObject.cursor = 0;
-            gameObject.gamePhase = 1;
+            gameObject.gamePhase = COMPUTER;
+            gameObject.level = 1
             $(document).off("keypress");
             break;
-        case "computer":
+        case COMPUTER:
+            $("#level-title").text("Level: " + gameObject.level);
             if (gameObject.cursor < gameObject.computerMoves.length) {
-                $("#" + gameObject.computerMoves[gameObject.cursor]).fadeOut(100).fadeIn(100);
-                let sample = new Audio("sounds/"+gameObject.computerMoves[gameObject.cursor]+".mp3");
-                sample.play()
+                // Sequentially replay computer moves
+                blink(gameObject.computerMoves[gameObject.cursor]);
+                playAudio(gameObject.computerMoves[gameObject.cursor]);
                 gameObject.cursor++
             } else {
-                gameObject.computerMoves.push(gameObject.moves[Math.trunc(Math.random() * gameObject.moves.length)]);
-                $("#" + gameObject.computerMoves[gameObject.cursor]).fadeOut(100).fadeIn(100);
-                gameObject.gamePhase = 2;
-                let sample = new Audio("sounds/"+gameObject.computerMoves[gameObject.cursor]+".mp3");
+                // Add new Move to computerMoves
+                gameObject.computerMoves.push(newMove());
+                blink(gameObject.computerMoves[gameObject.cursor]);
+                playAudio(gameObject.computerMoves[gameObject.cursor]);
+                // Transition to player Phase
                 gameObject.cursor = 0;
-                $("#level-title").text("Level: "+gameObject.level);
-                sample.play()
+                gameObject.gamePhase = PLAYER;
             }
             break;
-        case "player":
+        case PLAYER:
             $(".btn").on("click", (e) => {
                 gameObject.playerMoves.push(e.target.id);
-                $(e.target).toggleClass("pressed");
-                setTimeout(()=>{
-                    $(e.target).toggleClass("pressed");
-                },100);
-                // console.log(gameObject.cursor)
+                animateButtonPress(e.target);
                 if (gameObject.computerMoves[gameObject.cursor] == gameObject.playerMoves[gameObject.cursor]) {
-                    console.log(gameObject.computerMoves[gameObject.cursor] + "==" + gameObject.playerMoves[gameObject.cursor])
-                    let sample = new Audio("sounds/"+e.target.id+".mp3");
-                    sample.play();
+                    playAudio(e.target.id)
                     gameObject.cursor++;
                     if (gameObject.cursor == gameObject.computerMoves.length) {
                         $(".btn").off();
                         gameObject.playerMoves = []
-                        gameObject.gamePhase = 1;
+                        gameObject.gamePhase = COMPUTER;
                         gameObject.cursor = 0;
                         gameObject.level++;
                         setTimeout(gameStep, 800);
                     }
                 } else {
-                    let sample = new Audio("sounds/wrong.mp3");
-                    sample.play()
+                    playAudio();
                     $(".btn").off()
-                    gameObject.gamePhase = 3;
+                    gameObject.gamePhase = GAME_OVER;
                     setTimeout(gameStep, 500);
                 }
             })
             return;
         default:
-            gameObject.gamePhase=0;
-            gameObject.level=1;
             $("#level-title").html("You Lost!<br>Press any key to restart");
+            // Returns to Setup Phase
+            gameObject.gamePhase = SETUP;
             $(document).on("keypress", gameStep);
             return
-            break;
     }
+    // Chains next game loop
     setTimeout(gameStep, 500);
 }
-
 
 $(document).on("keypress", gameStep)
